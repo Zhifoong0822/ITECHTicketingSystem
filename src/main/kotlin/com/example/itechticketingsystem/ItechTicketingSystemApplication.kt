8b795045912f330d144cf274e934705878948851
Zhifoong0822 <zhifoong8827@gmail.com>
2026-04-13 11:13:35 +0800

    setup

---
+++ src/main/kotlin/com/example/itechticketingsystem/ItechTicketingSystemApplication.kt
@@ -0,0 +1,11 @@
+package com.example.itechticketingsystem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ItechTicketingSystemApplication
+
+fun main(args: Array<String>) {
+    runApplication<ItechTicketingSystemApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/itechticketingsystem/ItechTicketingSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.itechticketingsystem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ItechTicketingSystemApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
