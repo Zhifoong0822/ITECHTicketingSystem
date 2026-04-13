5d530dc33fe935f26c0b16e0f243d57a97c8e73d
Zhifoong0822 <zhifoong8827@gmail.com>
2026-04-13 11:43:21 +0800

    new setup

---
+++ src/main/kotlin/com/example/itechticketing/ItechTicketingApplication.kt
@@ -0,0 +1,11 @@
+package com.example.itechticketing
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ItechTicketingApplication
+
+fun main(args: Array<String>) {
+    runApplication<ItechTicketingApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/itechticketing/ItechTicketingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.itechticketing
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ItechTicketingApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
